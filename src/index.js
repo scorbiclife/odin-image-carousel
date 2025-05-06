cd0f3dbf3d1b40a98e7105652f62f8121fb52e96
scorbiclife <scorbiclife+git@gmail.com>
2025-05-06 18:40:17 +0900

    feat: add sample images

--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
 import "./index.css";
 
+// @ts-ignore
+import image1 from "../assets/Gemini_Generated_Image_nceq59nceq59nceq.jpeg";
+// @ts-ignore
+import image2 from "../assets/Gemini_Generated_Image_oi2swyoi2swyoi2s.jpeg";
+// @ts-ignore
+import image3 from "../assets/Gemini_Generated_Image_oi2swzoi2swzoi2s.jpeg";
+
+function imageComponent({ src, alt, width, height, ...attributes }) {
+  const image = document.createElement("img");
+  image.src = src;
+  image.alt = alt;
+  image.width = width;
+  image.height = height;
+  Object.entries(attributes).forEach(([attribute, value]) => {
+    image.setAttribute(attribute, value);
+  });
+  return image;
+}
+
+function placeholderImageComponent(imageSrc) {
+  return imageComponent({
+    src: imageSrc,
+    alt: "placeholder stock image",
+    width: 300,
+    height: 300,
+  });
+}
+
 function component() {
   const element = document.createElement("div");
-  element.innerHTML = "Webpack Template Setup Complete";
+  element.append(...[image1, image2, image3].map(placeholderImageComponent));
   return element;
 }
 
+// @ts-ignore
 document.getElementById("content").appendChild(component());
